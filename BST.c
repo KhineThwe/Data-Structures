d0761d5cd8917aa6192b10de807b8f9ed15bae1d
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-01-13 20:39:22 -0800

    BST with C

--- BST.c
+++ BST.c
@@ -1,35 +1,54 @@
-#include "stdio.h"
-#include "stdlib.h"
-struct node
-{
-	int data;
-	struct node *left;
-	struct node *right;
+// C program to demonstrate 
+// delete operation in binary
+// search tree
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int key;
+    struct node *left, *right;
 };
-void inorder(struct node *root)
-{
-	if(root != NULL)
-	{
-		inorder(root->left);
-		printf("  %d",root->data);
-		inorder(root->right);
-	}
-}
-// C function to search a given key in a given BST
-struct node* search(struct node* root, int data)
-{
-    // Base Cases: root is null or key is present at root
-    if (root == NULL || root->data == data)
-       return root;
 
-    // Key is greater than root's key
-    if (root->data < data)
-       return search(root->right, data);
-
-    // Key is smaller than root's key
-    return search(root->left, data);
+// A utility function to create a new BST node
+struct node* newNode(int item)
+{
+    struct node* temp
+            = (struct node*)malloc(sizeof(struct node));
+    temp->key = item;
+    temp->left = temp->right = NULL;
+    return temp;
 }
-/* Given a non-empty binary search
+
+// A utility function to do inorder traversal of BST
+void inorder(struct node* root)
+{
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->key);
+        inorder(root->right);
+    }
+}
+
+/* A utility function to 
+   insert a new node with given key in
+ * BST */
+struct node* insert(struct node* node, int key)
+{
+    /* If the tree is empty, return a new node */
+    if (node == NULL)
+        return newNode(key);
+
+    /* Otherwise, recur down the tree */
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else
+        node->right = insert(node->right, key);
+
+    /* return the (unchanged) node pointer */
+    return node;
+}
+
+/* Given a non-empty binary search 
    tree, return the node
    with minimum key value found in
    that tree. Note that the
@@ -42,10 +61,15 @@ struct node* minValueNode(struct node* node)
     while (current && current->left != NULL)
         current = current->left;
 
-    return current;//10
+    return current;
 }
 
-struct node* deleteNode(struct node* root, int data) {
+/* Given a binary search tree
+   and a key, this function
+   deletes the key and
+   returns the new root */
+struct node* deleteNode(struct node* root, int key)
+{
     // base case
     if (root == NULL)
         return root;
@@ -53,14 +77,14 @@ struct node* deleteNode(struct node* root, int data) {
     // If the key to be deleted
     // is smaller than the root's
     // key, then it lies in left subtree
-    if (data < root->data)
-        root->left = deleteNode(root->left, data);
+    if (key < root->key)
+        root->left = deleteNode(root->left, key);
 
         // If the key to be deleted
         // is greater than the root's
         // key, then it lies in right subtree
-    else if (data > root->data)
-        root->right = deleteNode(root->right, data);
+    else if (key > root->key)
+        root->right = deleteNode(root->right, key);
 
         // if key is same as root's key,
         // then This is the node
@@ -68,11 +92,12 @@ struct node* deleteNode(struct node* root, int data) {
     else {
         // node with only one child or no child
         if (root->left == NULL) {
-            struct node *temp = root->right;
+            struct node* temp = root->right;
             free(root);
             return temp;
-        } else if (root->right == NULL) {
-            struct node *temp = root->left;
+        }
+        else if (root->right == NULL) {
+            struct node* temp = root->left;
             free(root);
             return temp;
         }
@@ -80,85 +105,53 @@ struct node* deleteNode(struct node* root, int data) {
         // node with two children:
         // Get the inorder successor
         // (smallest in the right subtree)
-                struct node *temp =
-                 minValueNode(root->right);
+        struct node* temp = minValueNode(root->right);
 
         // Copy the inorder
         // successor's content to this node
-        root->data = temp->data;
+        root->key = temp->key;
 
         // Delete the inorder successor
-        root->right = deleteNode(root->right, temp->data);
-
-
-        return root;
+        root->right = deleteNode(root->right, temp->key);
     }
+    return root;
 }
-    int main()
-    {
-        struct node *root = NULL;
-        int n, i, item, data;
-        struct node *p, *q;
-        printf("Enter the number of nodes to be insert: ");
-        setbuf(stdout, 0);
-        scanf("%d", &n);
 
-        printf("\nPlease enter the numbers to be insert: ");
-        setbuf(stdout, 0);
-        for (i = 0; i < n; i++) {
-            p = (struct node *) malloc(sizeof(struct node));//every node you have to take memo location
-            scanf("%d", &item);
-            p->data = item;
-            p->left = NULL;
-            p->right = NULL;
+// Driver Code
+int main()
+{
+    /* Let us create following BST
+              50
+           /     \
+          30      70
+         /  \    /  \
+       20   40  60   80 */
+    struct node* root = NULL;
+    int n, i, item, data;
+    printf("Enter the number of nodes to insert: ");
+    setbuf(stdout, 0);
+    scanf("%d", &n);
 
-            if (i == 0) {
-                root = p;
-            } else {
-                q = root;   // q is used to traverse the tree
-                while (1) {
-                    if (p->data > q->data) {
-                        if (q->right == NULL) {
-                            q->right = p;
-                            break;
-                        } else {
-                            q = q->right;
-                        }
-                    } else {
-                        if (q->left == NULL) {
-                            q->left = p;
-                            break;
-                        } else {
-                            q = q->left;
-                        }
 
-                    }
-                }
-
-            }
-        }
-        printf("\nBinary Search Tree nodes in Inorder Traversal: ");
-        inorder(root);
-
-        printf("\nPlease enter the numbers to be search: ");
-        setbuf(stdout, 0);//for debugging purpose
-        scanf("%d", &data);
-        struct node *result = search(root, data);
-        if (result == NULL) {
-            printf("Element is not present in tree");
-        } else {
-            printf("Element is present in tree");
-        }
-
-        printf("\nPlease enter the numbers to be delete: ");
-        setbuf(stdout, 0);//for debugging purpose
-        scanf("%d", &data);
-        printf("\nAfter Deletion\n");
-        root = deleteNode(root, data);
-        printf("Inorder traversal of the modified tree \n");
-        inorder(root);
-
-        printf("\n");
-        return 0;
+    /* Let us insert
+         50,30,20,40,70,60,80 */
+    printf("\nPlease enter the numbers to insert: ");
+    setbuf(stdout, 0);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &item);
+        root = insert(root, item);
     }
-//Implementaion of Binary Search Tree with C
+
+    printf("Inorder traversal of the given tree \n");
+    inorder(root);
+
+    printf("\nPlease enter the numbers to delete: ");
+    setbuf(stdout, 0);//for debugging purpose
+    scanf("%d", &data);
+    printf("\nAfter Deleting\n");
+    root = deleteNode(root, data);
+    printf("Inorder traversal of the modified tree \n");
+    inorder(root);
+    return 0;
+}
+//Implementaion of BST with C
